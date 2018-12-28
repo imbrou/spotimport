d6c1d517bf8bb44093000acdbcaf2c3995c2bfe0
Nicolas Broussard <nicolas@skale-5.com>
2018-12-28 02:08:40 +0100

    parsed file + searched for first track + returned first result

--- app.js
+++ app.js
@@ -1,46 +1,81 @@
-var Spotify = require('spotify-web-api-node'),
-         fs = require('fs'),
-       path = require('path'),
-          s ;
+var SpotifyWebApi = require('spotify-web-api-node'),
+                s = null ;
 
 /**
- * Init Spotify API.
+ * Initializes Spotify API.
+ * 
+ * @return an authenticated Spotify API instance
  */
-var initSpotify = function initSpotify () {
+var initSpotify = async function initSpotify () {
 	if ( s === null ) {
-		s = new Spotify({
+		// Instanciate library
+		var api = new SpotifyWebApi({
 			clientId: 'dc3a0ffddcd64f0b94f57dbcbed0b27f',
 			clientSecret: '863b2bbfa8634b74a888b5b04f6dea24',
-			redirectUri: 'http://localhost:3000/callback'
+			//redirectUri: 'http://localhost:3000/callback'
 		}) ;
+		// Authenticate against Spotify's OAuth servers
+		try {
+			let data = await api.clientCredentialsGrant() ;
+			api.setAccessToken(data.body['access_token']) ;
+			s = api ;
+		} catch (err) {
+			console.log(
+				'Something went wrong when retrieving an access token',
+				err.message
+			) ;
+		} ;
 	}
 	return s ;
 }
 
 /**
- * Print usage.
+ * Prints usage.
  */
 var printUsage = function printUsage () {
 	console.log("\nUsage:\n\tnode app.js <playlist.txt>\n") ;
 }
 
+/**
+ * Loads tracks of a playlist from a TXT file.
+ * Tracks must be newline-separated.
+ * On a line, the artist must come first, then the ' - ' separator, then the title.
+ * 
+ * @param filename {String} path to the file to open
+ * @return {String} playlist
+ */
 var loadPlaylist = function loadPlaylist (filename) {
-	return new Promise(function(resolve, reject) {
-		fs.readFile(filename, {encoding: 'utf-8'}, function (err,data) {
-			if (!err) {
-				resolve(data) ;
-			} else {
-				reject(err) ;
-			}
+	let file = require('fs').readFileSync(filename, {encoding: 'utf-8'}) ;
+	var playlist = file.split('\n')
+		.map( function (line, index) {
+			return {
+				index: index,
+				artist: line.split(' - ').shift().trim(),
+				title: line.split(' - ').pop().trim()
+			} ;
 		}) ;
-	});
+	return playlist ;
 }
 
-var firstTrackResultFor = function firstTrackResultFor (queryTerm) {
-	s.searchTracks(queryTerm, {limit: 5})
-		.then( function (data) {
-			console.log( JSON.stringify(data,null,2) ) ;
-		}) ;
+/**
+ * Returns the first track result for a given query string.
+ * 
+ * @param queryTerm {String} the query string
+ * @return the first track result
+ */
+var firstTrackResultFor = async function firstTrackResultFor (queryTerm) {
+	var track = null ;
+	try {
+		let data = await s.searchTracks(queryTerm, {limit: 5}) ;
+		track = data.body.tracks.items[0] ;
+		console.log("Found:") ;
+		console.log(`\tName: ${track.name}`) ;
+		console.log(`\tArtist: ${track.artists[0].name}`) ;
+		console.log(`\tURI: ${track.uri}`) ;
+	} catch (err) {
+		console.log( 'ERROR: ' + JSON.stringify(err,null,2) ) ;
+	}
+	return track ;
 }
 
 if ( process.argv.length < 3) {
@@ -48,16 +83,17 @@ if ( process.argv.length < 3) {
 	return 1 ;
 }
 
-process.stdout.write(">>> Initializing API…") ;
-initSpotify() ;
-process.stdout.write(" done!\n") ;
+console.log(">>> Initializing API... ") ;
+var promise = initSpotify() ;
+console.log(">>> Loading playlist... ") ;
+var playlist = loadPlaylist(process.argv[2]) ;
 
-process.stdout.write(">>> Loading file…") ;
-var promise = loadPlaylist(process.argv[2]) ;
-process.stdout.write(" done!\n") ;
-promise.then( function (playlist) {
-	console.log(playlist) ;
+promise.then( function () {
+	var track = playlist.shift() ;
+	track.artist = track.artist.replace(';','') ;
+
+	console.log(`>>> Searching for "${track.artist} ${track.title}"...`) ;
+	firstTrackResultFor( `${track.artist} ${track.title}` ) ;
 }) ;
-//console.log(playlist) ;
 
 return 0 ;
